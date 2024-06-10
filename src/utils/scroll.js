09d48cc40c6d20dce6b6755651f9622d5e84140d
pandaman404 <valenzuela.ahumada4@gmail.com>
2024-06-09 21:23:49 -0400

    feat: meta tags

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,16 +1,16 @@
 export const scrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 };
 
 export const scrollIntoViewWithOffset = (id, offset = 0) => {
-    window.scrollTo({
-        behavior: 'smooth',
-        top:
-            document.getElementById(id).getBoundingClientRect().top -
-            document.body.getBoundingClientRect().top -
-            offset,
-    });
+  window.scrollTo({
+    behavior: 'smooth',
+    top:
+      document.getElementById(id).getBoundingClientRect().top -
+      document.body.getBoundingClientRect().top -
+      offset,
+  });
 };
